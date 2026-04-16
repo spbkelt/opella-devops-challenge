e42ae7d7697d890ae9b34b39ddd200f22236ba6c
Dmitry Zolotnikov <spbkelt@gmail.com>
2026-04-16 04:01:48 +0200

    fix: Add missing providers

--- environments/prod/versions.tf
+++ environments/prod/versions.tf
@@ -1,10 +1,19 @@
 terraform {
   required_version = ">= 1.5.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.116"
+      version = "~> 3.117"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
     }
   }
 
